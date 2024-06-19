697702fbbdc97b24c75d1c27d3964255fd2c3a1a
crsrcrsr <102668692+crsrcrsr@users.noreply.github.com>
2024-06-19 15:52:26 +0700

    Add files via upload

---
+++ blocks.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" Mem = ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30,		0},
+	{" T% = ", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" Bat = ", "acpi | awk '{print $4}' | sed s/,// ",                            60,             0}, 
+	{" CRSRCRSRRR ","", 60,0 } ,
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " •";
+static unsigned int delimLen = 5;
